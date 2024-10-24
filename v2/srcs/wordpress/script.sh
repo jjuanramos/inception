ddf51251b57153411b2a347c71d527867168d98d
jjuanramos <jjuanramos91@gmail.com>
2024-10-24 16:32:24 +0200

    beautified

--- v2/srcs/wordpress/script.sh
+++ v2/srcs/wordpress/script.sh
@@ -7,11 +7,17 @@ else
 	curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 	chmod +x wp-cli.phar
 	./wp-cli.phar core download --allow-root
-	./wp-cli.phar config create --dbname=${db_name:-wordpress} \
+	./wp-cli.phar config create \
+		--dbname=${db_name:-wordpress} \
         --dbuser=${db_user:-wpuser} \
         --dbpass=${db_pwd:-password} \
 		--dbhost=mariadb --allow-root
-	./wp-cli.phar core install --url=localhost --title=inception --admin_user=admin --admin_password=admin --admin_email=admin@admin.com --allow-root
+	./wp-cli.phar core install \
+		--url=localhost \
+		--title=inception \
+		--admin_user=admin \
+		--admin_password=admin \
+		--admin_email=admin@admin.com --allow-root
 fi
 
 /usr/sbin/php-fpm7.3 -F
