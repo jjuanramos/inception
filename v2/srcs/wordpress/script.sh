af92e968d764a3b66296362db8f384028fac4636
jjuanramos <jjuanramos91@gmail.com>
2024-10-25 11:19:55 +0200

    there was a typo man

--- v2/srcs/wordpress/script.sh
+++ v2/srcs/wordpress/script.sh
@@ -4,13 +4,16 @@ if [ -f wp-config.php ]
 then
 	echo "wp-config.php file found"
 else
+	echo "Database Name: ${db_name}"
+	echo "Database User: ${db_user}"
+	echo "Database Password: ${db_pwd}"
 	curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 	chmod +x wp-cli.phar
 	./wp-cli.phar core download --allow-root
 	./wp-cli.phar config create \
-		--dbname=${db_name:-wordpress} \
-        --dbuser=${db_user:-wpuser} \
-        --dbpass=${db_pwd:-password} \
+		--dbname=${db_name} \
+        --dbuser=${db_user} \
+        --dbpass=${db_pwd} \
 		--dbhost=mariadb --allow-root
 	./wp-cli.phar core install \
 		--url=localhost \
