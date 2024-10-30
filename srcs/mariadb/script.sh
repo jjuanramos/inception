9d764da2ab153e376da2a42ecdb1531e6cd5f866
juramos <juramos@student.42madrid.com>
2024-10-30 12:11:08 +0100

    added .env, scritp.sh

--- srcs/mariadb/script.sh
+++ srcs/mariadb/script.sh
@@ -11,14 +11,18 @@ until mysqladmin ping --silent; do
 	sleep 2
 done
 
-echo "CREATE DATABASE IF NOT EXISTS $db_name;" >> db1.sql
-echo "CREATE USER IF NOT EXISTS '$db_user'@'%' IDENTIFIED BY '$db_pwd';" >> db1.sql
-echo "GRANT ALL PRIVILEGES ON *.* TO '$db_user'@'%' WITH GRANT OPTION;" >> db1.sql
-echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$db_root_pwd' ;" >> db1.sql
-echo "FLUSH PRIVILEGES;" >> db1.sql
+if [ ! -f db1.sql ]; then
+    echo "CREATE DATABASE IF NOT EXISTS $db_name;" >> db1.sql
+    echo "CREATE USER IF NOT EXISTS '$db_user'@'%' IDENTIFIED BY '$db_pwd';" >> db1.sql
+    echo "GRANT ALL PRIVILEGES ON *.* TO '$db_user'@'%' WITH GRANT OPTION;" >> db1.sql
+    echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$db_root_pwd' ;" >> db1.sql
+    echo "FLUSH PRIVILEGES;" >> db1.sql
 
-mysql -u root < db1.sql || echo "Failed to execute SQL script."
-
-mysqladmin -u root -p"$db_root_pwd" shutdown || echo "Failed to stop MySQL."
+    mysql -u root < db1.sql || echo "Failed to execute SQL script."
+	
+	mysqladmin -u root -p"$db_root_pwd" shutdown || echo "Failed to stop MySQL."
+else
+    echo "Database already initialized; skipping SQL execution."
+fi
 
 exec mysqld_safe || echo "Failed to start mysqld."
